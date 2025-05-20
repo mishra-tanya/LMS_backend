1e9bdfc8b1b49d990527ed89d3711484394b7acf
Tanya Mishra <tanyakrishanmishra@gmail.com>
2025-05-20 12:24:33 +0530

    commented factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,10 +19,10 @@ class DatabaseSeeder extends Seeder
         
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
         
         // Call seeders in the correct order to maintain referential integrity
         $this->call([
