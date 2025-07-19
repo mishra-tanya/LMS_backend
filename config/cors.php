44618c92b5f6c23092d90fa6eb5c9c51496211d1
Tanya Mishra <tanyakrishanmishra@gmail.com>
2025-07-19 21:23:52 +0530

    minor issue resolved

--- config/cors.php
+++ config/cors.php
@@ -6,8 +6,7 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'http://localhost:8080',
-        'https://amplifilearn.com',
+       '*'
     ],
 
     'allowed_origins_patterns' => [],
